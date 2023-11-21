da4a57a049e7d50cdfb96c016f02f010a6df6f3d
Raffael Sahli <raffael.sahli@doodle.com>
2023-11-21 09:27:16 +0100

    refactor: metric names as suggested here https://github.com/prometheus/docs/pull/2386

--- internal/collector/metrics.go
+++ internal/collector/metrics.go
@@ -4,22 +4,22 @@ import "github.com/prometheus/client_golang/prometheus"
 
 var (
 	notBefore = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "saml_x509_cert_not_before",
+		Name: "saml_x509_cert_not_valid_before",
 		Help: "SAML X509 certificate validity period start",
 	}, []string{"entityid", "use", "serial_number", "issuer_C", "issuer_CN", "issuer_L", "issuer_O", "issuer_ST", "subject_C", "subject_CN", "subject_L", "subject_O"})
 
 	notAfter = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "saml_x509_cert_not_after",
+		Name: "saml_x509_cert_not_valid_after",
 		Help: "SAML X509 certificate expiration date",
 	}, []string{"entityid", "use", "serial_number", "issuer_C", "issuer_CN", "issuer_L", "issuer_O", "issuer_ST", "subject_C", "subject_CN", "subject_L", "subject_O"})
 
 	certParse = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "saml_x509_read_error_total",
+		Name: "saml_x509_read_errors_total",
 		Help: "Errors encountered while parsing SAML X509 certificates",
 	}, []string{"entityid", "use"})
 
 	metadataParse = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "saml_metadata_error_total",
+		Name: "saml_metadata_errors_total",
 		Help: "Errors encountered while parsing SAML metadata",
 	}, []string{"url"})
 )
